1a942753940289b1c51b4b930d5393dc2d327002
ushloo <ashleyjane377@gmail.com>
2024-11-18 20:47:04 -0500

    buttoning and forming

--- notes.js
+++ notes.js
@@ -1,3 +1,11 @@
-function balls(){
-    alert("big balls");
-}
+const addNoteButton = document.querySelector(".add_note_button")
+const noteForm = document.querySelector(".modal")
+const closeButton = document.querySelector(".modal__close-button")
+
+addNoteButton.addEventListener('click', () => {
+    noteForm.classList.add("modal_opened")
+})
+
+closeButton.addEventListener('click', () => {
+    noteForm.classList.remove("modal_opened")
+})
