708cfd34328e31f8b0a2f637f91623948facc43b
ushloo <ashleyjane377@gmail.com>
2024-11-18 21:34:31 -0500

    button clicking and escaping

--- notes.js
+++ notes.js
@@ -2,10 +2,30 @@ const addNoteButton = document.querySelector(".add_note_button")
 const noteForm = document.querySelector(".modal")
 const closeButton = document.querySelector(".modal__close-button")
 
-addNoteButton.addEventListener('click', () => {
-    noteForm.classList.add("modal_opened")
-})
+// function handleEsc(){
+//     if (e.key === "Escape") {
+//         noteForm.classList.remove("modal_opened")
+//     }
+// }
 
-closeButton.addEventListener('click', () => {
-    noteForm.classList.remove("modal_opened")
-})
+
+
+function close() {
+    document.removeEventListener("keydown", handleEsc);
+    noteForm.classList.remove("modal_opened");
+}
+  
+function handleEsc(e) {
+    if (e.key === "Escape") {
+      close();
+    }
+}
+
+function open() {
+    noteForm.classList.add("modal_opened");
+    document.addEventListener("keydown", handleEsc);
+}
+  
+addNoteButton.addEventListener("click", open); 
+closeButton.addEventListener("click", close);
+
